e4aa93c318e3f8785a95edbe6ed33b7dbadff77d
Dmitry Karavaev <dkaravaev@stc-spb.ru>
2019-10-15 10:02:09 +0300

    Fixed error in lecture 1.

--- Part1/Projects/Convolution/source/convolve.c
+++ Part1/Projects/Convolution/source/convolve.c
@@ -42,5 +42,22 @@ int DSP_convolve_decimate(const float* xh, float* y, size_t N,
 int DSP_convolve_complex_int(const int16_t* xh, int16_t* y, size_t N, 
                              const int16_t* h,  size_t T)
 {
-    return -1;
+    int32_t value[2];
+
+    const int16_t* x = xh + 2 * (T - 1);
+    for (size_t n = 0; n < N; ++n)
+    {
+        value[0] = 0;
+        value[1] = 0;
+        for (size_t k = 0; k < T; ++k)
+        {
+            value[0] += h[k] * x[2 * (n - k)];
+            value[1] += h[k] * x[2 * (n - k) + 1];
+        }
+
+        y[2 * n]     = value[0] >> 15;
+        y[2 * n + 1] = value[1] >> 15;
+    }
+
+    return 0;
 }
